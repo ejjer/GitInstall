f927c0177eec810ad24f66fdf3df80d1c51540ec
ThinkPad T470 <belkoegor1@gmail.com>
2023-07-11 10:31:41 +0300

    mainA

--- app/src/main/java/com/example/gitinstall/MainActivity.kt
+++ app/src/main/java/com/example/gitinstall/MainActivity.kt
@@ -13,34 +13,5 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.gitinstall.ui.theme.GitInstallTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            GitInstallTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GitInstallTheme {
-        Greeting("Android")
-    }
 }
